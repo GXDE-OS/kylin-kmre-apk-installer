a85ce89854d57da589cfa13b6a2cc74d2d76c8be
gfdgd_xi <3025613752@qq.com>
2026-06-10 13:31:55 +0800

    修复双标题栏问题；优化拖动事件
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -345,61 +345,7 @@ void MainWindow::mouseMoveEvent(QMouseEvent *e)
         return;
 
     qreal  dpiRatio = qApp->devicePixelRatio();
-    if (QGuiApplication::platformName() == "xcb") {
-        Display *display = XOpenDisplay(nullptr);
-        if (display) {
-            Atom netMoveResize = XInternAtom(display, "_NET_WM_MOVERESIZE", False);
-            XEvent xEvent;
-            const auto pos = QCursor::pos();
-
-            memset(&xEvent, 0, sizeof(XEvent));
-            xEvent.xclient.type = ClientMessage;
-            xEvent.xclient.message_type = netMoveResize;
-            xEvent.xclient.display = display;
-            xEvent.xclient.window = this->winId();
-            xEvent.xclient.format = 32;
-            xEvent.xclient.data.l[0] = pos.x() * dpiRatio;
-            xEvent.xclient.data.l[1] = pos.y() * dpiRatio;
-            xEvent.xclient.data.l[2] = 8;
-            xEvent.xclient.data.l[3] = Button1;
-            xEvent.xclient.data.l[4] = 0;
-
-            XUngrabPointer(display, CurrentTime);
-            
-            Window rootWindow = DefaultRootWindow(display);
-            XSendEvent(display, rootWindow,
-                       False, SubstructureNotifyMask | SubstructureRedirectMask,
-                       &xEvent);
-            //XFlush(display);
-
-            XEvent xevent;
-            memset(&xevent, 0, sizeof(XEvent));
-
-            xevent.type = ButtonRelease;
-            xevent.xbutton.button = Button1;
-            xevent.xbutton.window = this->winId();
-            xevent.xbutton.x = e->pos().x() * dpiRatio;
-            xevent.xbutton.y = e->pos().y() * dpiRatio;
-            xevent.xbutton.x_root = pos.x() * dpiRatio;
-            xevent.xbutton.y_root = pos.y() * dpiRatio;
-            xevent.xbutton.display = display;
-
-            XSendEvent(display, this->effectiveWinId(), False, ButtonReleaseMask, &xevent);
-            XFlush(display);
-            XCloseDisplay(display);
-        }
-
-        if (e->source() == Qt::MouseEventSynthesizedByQt) {
-            if (!this->mouseGrabber()) {
-                this->grabMouse();
-                this->releaseMouse();
-            }
-        }
-
-        m_is_draging = false;
-    } else {
-        this->move((QCursor::pos() - m_offset) * dpiRatio);
-    }
+    this->move((QCursor::pos() - m_offset) * dpiRatio);
 }
 
 void MainWindow::mouseReleaseEvent(QMouseEvent *e)
